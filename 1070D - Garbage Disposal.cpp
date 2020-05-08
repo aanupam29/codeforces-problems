b404da201d9578d3a4ae9e98eaee85eed93ddbf3
Anupam Agrawal <58875579+aanupam29@users.noreply.github.com>
2020-05-08 23:53:32 +0530

    Update 1070D - Garbage Disposal.cpp

--- 1070D - Garbage Disposal.cpp
+++ 1070D - Garbage Disposal.cpp
@@ -1,19 +1,5 @@
 #include<bits/stdc++.h>
 typedef long long int ll;
-typedef unsigned long long int ull;
-#define dbg printf("in\n");
-#define nl printf("\n");
-#define N 200100
-#define inf 1e18
-#define sf(n) scanf("%d", &n)
-#define sff(n,m) scanf("%d%d",&n,&m)
-#define sfl(n) scanf("%I64d", &n)
-#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
-#define pf(n) printf("%d",n)
-#define pff(n,m) printf("%d %d",n,m)
-#define pffl(n,m) printf("%I64d %I64d",n,m)
-#define pfl(n) printf("%I64d",n)
-#define pfs(s) printf("%s",s)
 using namespace std;
 int main()
 {
